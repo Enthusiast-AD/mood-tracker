206425ae0686010a1a6370da46165f8fb6469063
Ansh Deep <ad0811@srmist.edu.in>
2025-07-07 20:53:33 +0530

    .env.sample update

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-55d1eca6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6ikd3mv4vlo"
+    "revision": "0.0soj03ugrrg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
